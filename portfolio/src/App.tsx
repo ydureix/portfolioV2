feca819dcb1157edf840225abe97eb1c581314eb
Ydureix <rudy11923@gmail.com>
2025-06-12 16:15:00 -0700

    Implement Header

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex flex-col md:flex-row'>
-        <p> Hello</p>
-        <p> Rudy</p>
-      </div>
+      <Header></Header>
     </>
   )
 }
---
+++ portfolio/src/components/header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full bg-neutral-800 text-green-50 font-mono p-4 shadow">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <span className="text-xl font-bold border-b border-r pr-1 ">Rudy Xie</span>
+        <nav className="flex space-x-6">
+          <span className="hover:underline cursor-pointer">About</span>
+          <span className="hover:underline cursor-pointer">Projects</span>
+          <span className="hover:underline cursor-pointer">Contact</span>
+        </nav>
+      </div>
+    </header>
+  );
+}

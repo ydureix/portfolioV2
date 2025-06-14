5c946c6150c6689a1b41f2dfd5cdfbfc53d3a684
Ydureix <rudy11923@gmail.com>
2025-06-13 20:02:25 -0700

    Implement AboutMe

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,15 @@
 import Header from './components/header'
 import './App.css'
+import AboutMe from './components/aboutme'
 
 function App() {
 
   return (
     <>
+    <div className='bg-neutral-800'>
       <Header></Header>
+      <AboutMe />
+    </div>
     </>
   )
 }
--- portfolio/src/components/aboutme.tsx
+++ portfolio/src/components/aboutme.tsx
@@ -1,1 +1,11 @@
-
+export default function AboutMe() {
+  return (
+    <div className="flex justify-center mt-6">
+      <div className="flex flex-col gap-2 bg-neutral-800 p-6 rounded-lg w-80 md:w-190 sm:w-150 border-blue-100  border-1">
+        <h1 className="text-green-50 font-mono text-3xl">Rudy Xie</h1>
+        <p className="text-neutral-300 font-mono">Fullstack Developer</p>
+        <p className="text-green-50 font-mono">Hello! I am Rudy, a fullstack developer with 4+ years of experience programming. I am currently pursuing a Bachelor's of Science for Software Engineer at the University of California, Irvine. </p>
+      </div>
+    </div>
+  );
+}

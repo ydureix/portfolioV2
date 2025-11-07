d57af61dc915e32cef30e1dffe3fe430b0758a3c
ydureix <rudy11923@gmail.com>
2025-11-07 10:42:23 -0800

    Update Project and experience

--- portfolio/src/components/experience.tsx
+++ portfolio/src/components/experience.tsx
@@ -17,32 +17,26 @@ export default function Experience() {
         <h1 className="text-green-50 font-mono font-extrabold text-5xl pb-5">Experience</h1>
       </div>
       <div className="flex flex-col">
-        {/* vertical connecting line */}
-        <div className="flex  bg-emerald-400" />
-
         {timelineData.map((item, index) => (
-          <motion.div
+          <div
             key={index}
-            className="relative mb-10 pl-16"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: index * 0.2, duration: 0.6 }}
+            className="relative md:mb-10 md:pl-16"
           >
             {/* Dot on the line */}
-            <div className="absolute left-6 top-10 w-5 h-5 bg-emerald-400 rounded-full border-4 border-white z-10" />
-            
+            <div className="hidden md:block md:absolute left-4 top-10 w-5 h-5 bg-emerald-400 rounded-full border-4 border-white z-10" />
+
             {/* Content */}
-            <div className="bg-neutral-800 text-neutral-100 p-4 rounded-lg shadow">
+            <div className="flex flex-col justify-between bg-neutral-800 text-neutral-100 rounded-lg shadow">
               <div className="mb-3">
-              <div className="flex flex-row justify-between mb-0.4">
-                <h3 className="font-semibold text-2xl">{item.company}</h3>
+              <div className="flex flex-row sm:justify-between mb-0.4">
+                <h3 className="font-semibold text-2xl">{item.role}</h3>
                 <time className="block text-sm text-gray-400">{item.date}</time>
               </div>
-                <h2 className="italic">{item.role}</h2>
+                <h2 className="italic">{item.company}</h2>
               </div>
               <p className="mt-1 text-base">{item.description}</p>
             </div>
-          </motion.div>
+          </div>
         ))}
       </div>
     </div>
